461545cb703ea1acc7f3e8684707703f781ad807
Anusa R <60094900+ANUSA24@users.noreply.github.com>
2022-06-15 11:33:38 +0530

    Add files via upload

--- Sum.java
+++ Sum.java
@@ -1,16 +1,16 @@
 import java.util.*;
 class Sum
 {
-	public static void main(String[] args)
-	{
-		Scanner sc= new Scanner(System.in);
-		System.out.print("Enter the Ist Number:-");
-		int a=sc.nextInt();
-		System.out.print("Enter the IInd Number:-");
-                int b=sc.nextInt();
-		System.out.print("Enter the IIIrd Number:-");
-                int c=sc.nextInt();
-		int d=a+b+c;
-		System.out.println("Total ="+d);
-	}
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter first number: ");
+int a=sc.nextInt();
+System.out.print("Enter second number: ");
+int b=sc.nextInt();
+System.out.print("enter thrid number: ");
+int c=sc.nextInt();
+int d=a+b+c;
+System.out.println("Total= "+d);
+}
 }
