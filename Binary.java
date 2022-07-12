9a664b7e172f362a033e0c32112ff14dfaa7f3f6
Anusa R <60094900+ANUSA24@users.noreply.github.com>
2022-07-12 21:03:59 +0530

    Create Binary.java

---
+++ Binary.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Binary{  
+ public static void binarySearch(int arr[], int first, int last, int key){  
+   int mid = (first + last)/2;  
+   while( first <= last ){  
+      if ( arr[mid] < key ){  
+        first = mid + 1;     
+      }else if ( arr[mid] == key ){  
+        System.out.println("Element is found at index: " + mid);  
+        break;  
+      }else{  
+         last = mid - 1;  
+      }  
+      mid = (first + last)/2;  
+   }  
+   if ( first > last ){  
+      System.out.println("Element is not found!");  
+   }  
+ }  
+ public static void main(String args[]){  
+        int arr[],key,last,n,i;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of elements");
+        n=sc.nextInt();
+        arr=new int[n];
+        System.out.println("Enter sorted arrays");
+        for(i=0;i<n;i++)
+           arr[i]=sc.nextInt();
+        System.out.println("Enter the element to be serached");
+        key=sc.nextInt();  
+        binarySearch(arr,0,n-1,key);     
+ }  
+}  
